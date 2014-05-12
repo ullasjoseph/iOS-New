5ba7ee3293d620891698126c843a720ec8143610
samaygrover <samaygrover@gmail.com>
2014-05-12 18:31:02 -0400

    First Commit

---
+++ SampleClass.h
@@ -0,0 +1,13 @@
+//
+//  SampleClass.h
+//  FirstGitHubProject
+//
+//  Created by macadmin on 2014-05-12.
+//  Copyright (c) 2014 macadmin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleClass : NSObject
+
+@end
---
+++ SampleClass.m
@@ -0,0 +1,14 @@
+//
+//  SampleClass.m
+//  FirstGitHubProject
+//
+//  Created by macadmin on 2014-05-12.
+//  Copyright (c) 2014 macadmin. All rights reserved.
+//
+
+#import "SampleClass.h"
+
+@implementation SampleClass
+
+
+@end
